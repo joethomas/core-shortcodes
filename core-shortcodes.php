3e3dc8c946450c2fe5fde4b72e22a573567e6c4e
Joe Thomas <joe@Joes-MBP.local>
2018-01-03 13:27:12 -0600

    v1.1.0 - Added two new shortcodes

--- core-shortcodes.php
+++ core-shortcodes.php
@@ -3,14 +3,14 @@
 	Plugin Name: Core Shortcodes
 	Description: This plugin defines shortcodes that need to persist (even if the WordPress theme is changed in the future).
 	Plugin URI: https://github.com/joethomas/core-shortcodes
-	Version: 1.0.1
+	Version: 1.1.0
 	Author: Joe Thomas
 	Author URI: https://github.com/joethomas
 	License: GNU General Public License v3.0
 	License URI: http://www.gnu.org/licenses/gpl-3.0.html
 	Text Domain: ultimate-click-to-copy
 	Domain Path: /languages/
-	
+
 	GitHub Plugin URI: https://github.com/joethomas/core-shortcodes
 	GitHub Branch: master
 */
@@ -52,3 +52,70 @@ function joe_cs_shortcode_site_name() {
 
 }
 add_shortcode( 'site_name', 'joe_cs_shortcode_site_name' );
+
+/**
+ * Scroll Down Arrow
+ *
+ * Usage: [joe_scroll_down_arrow css_animation="bottom-t-top" link="url:#nextsection]
+ *
+ * @since 1.1.0
+ */
+function joe_cs_shortcode_scroll_down_arrow( $atts ) {
+
+	extract(shortcode_atts(
+		array(
+			'css_animation' => 'bottom-to-top',
+			'link' => '#',
+		),
+		$atts
+	) );
+
+	switch ( $css_animation ) {
+	    case 'none':
+	        $animation = '';
+	        break;
+	    case 'top-to-bottom':
+	        $animation = ' animate_when_almost_visible top-t-bottom start_animation';
+	        break;
+	    case 'bottom-to-top':
+	        $animation = ' animate_when_almost_visible bottom-t-top start_animation';
+	        break;
+	}
+
+	if ( 'none' !== $css_animation ) {
+		wp_enqueue_script( 'waypoints' );
+	}
+
+	$output = '<div class="joe-scrolldown' . $animation . '">';
+	$output .= '<a href="' . $link . '"><i class="fa fa-angle-down"></i></a>';
+	$output .= '</div>';
+	$output .= '</span>';
+
+	return $output;
+
+}
+add_shortcode( 'joe_scroll_down_arrow', 'joe_cs_shortcode_scroll_down_arrow' );
+
+/**
+ * Custom Field
+ *
+ * Usage: [custom_field id="custom_field_name"]
+ *
+ * @link http://wpexplorer-themes.com/total/snippets/meta-shortcode/
+ * @since 1.1.0
+ */
+function joe_cs_shortcode_custom_field( $atts ) {
+	$atts = extract( shortcode_atts( array(
+		'id' => '',
+		'format' => 'full', // full, raw
+	), $atts ) );
+	if ( ! $id ) return;
+	$id   = $id; // prefix the id
+	$data = get_post_meta( get_the_ID(), $id, true );
+	if ( $data && 'full' == $format )  {
+		return '<span class="custom-field id-'. $id .'">'. $data .'</span>';
+	} else if ( $data && 'raw' == $format ) {
+		return $data;
+	}
+}
+add_shortcode( 'custom_field', 'joe_cs_shortcode_custom_field' );
